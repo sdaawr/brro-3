5a5a9300d531c296f0ad4298c2fd140cc1118624
sdaawr <45720212+sdaawr@users.noreply.github.com>
2019-06-29 09:19:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,10 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("594491787842945055")
+let channel =     client.channels.get("594557196046696458")
 setInterval(function() {
-channel.send(`I Need credits Hh bm hhbm hhbm hhbm cretors by devil I Need credits Hh bm hhbm hhbm hhbm cretors by devil`);
+channel.send(`Heyyyyyyy I'm here. I'm here to spam texting or writing or typing or something like that I don't know why I'm here but hell yeah I like this place for reason I don't know what is it wooooooo weeeeeeee waaaaaaa it's party timeeee with spaming
+`);
 }, 30)
 })
 
